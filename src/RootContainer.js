e8aa49a88618d5249bb707970cb1fab5f700084b
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-19 00:56:06 +0530

    called query function and setting api response to state variable

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { queryData } from './query';
 
-class RootContainer extends React.Component {
-	render() {
-		return (
-			<div className="rootContainer">
-				<h1>Your Data Viz Here</h1>
+const RootContainer = ({ serviceUrl, entity }) => {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		setLoading(true);
+		const { value } = entity;
+		queryData({
+			serviceUrl: serviceUrl,
+			geneId: !Array.isArray(value) ? [value] : value
+		}).then(data => {
+			setData(data);
+			setLoading(false);
+		});
+	}, []);
+
+	return (
+		<div className="rootContainer">
+			<div className="innerContainer">
+				<div className="graph">
+					<span className="chart-title">Gene Tissue Localisation Network</span>
+					{!data.length && loading ? <h1>Loading...</h1> : <></>}
+				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default RootContainer;
