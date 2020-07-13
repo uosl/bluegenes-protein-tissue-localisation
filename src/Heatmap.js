6dfe2ac67d146283fc9107b50ca017d4a3f3a6f4
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-13 13:26:10 +0530

    added space in between multiple heatmap

--- src/Heatmap.js
+++ src/Heatmap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HeatMap } from '@nivo/heatmap';
-const heatmap_colors = ['#edf8e9', '#bae4b3', '#74c476', '#238b45'];
+const heatmap_colors = ['#B9D6F7', '#87B8EF', '#569EEE', '#1A75DC'];
 
 const Heatmap = ({ graphData, graphHeight }) => {
 	return (
@@ -13,36 +13,44 @@ const Heatmap = ({ graphData, graphHeight }) => {
 				overflowY: 'hidden'
 			}}
 		>
-			{graphData.map(g =>
-				Object.keys(g).map((d, index) => (
+			{graphData.map(data =>
+				Object.keys(data).map((item, index) => (
 					<div
 						key={index}
 						style={{
 							width:
 								index != 0
-									? Object.values(g[d].tissue).length * 50
-									: Object.values(g[d].tissue).length * 50 + 60,
+									? Object.values(data[item].tissue).length * 50 + 100
+									: Object.values(data[item].tissue).length * 50 + 160,
 							height: graphHeight
 						}}
 					>
 						<HeatMap
-							data={Object.values(g[d].data)}
-							keys={Object.values(g[d].tissue).map(t => t.value)}
+							data={Object.values(data[item].data)}
+							keys={Object.values(data[item].tissue).map(t => t.value)}
 							colors={heatmap_colors}
 							indexBy={'Gene'}
-							margin={{ top: 200, right: 0, bottom: 0, left: !index ? 60 : 0 }}
+							margin={{
+								top: 200,
+								right: 0,
+								bottom: 0,
+								left: index != 0 ? 0 : 60
+							}}
 							forceSquare={true}
 							axisTop={{
 								orient: 'top',
 								tickSize: 5,
 								tickPadding: 5,
-								tickRotation: -90
+								tickRotation: -90,
+								legendOffset: -160,
+								legendPosition: 'left',
+								legend: item
 							}}
 							height={graphHeight}
 							width={
 								index != 0
-									? Object.values(g[d].tissue).length * 50
-									: Object.values(g[d].tissue).length * 50 + 60
+									? Object.values(data[item].tissue).length * 50 + 100
+									: Object.values(data[item].tissue).length * 50 + 160
 							}
 							axisRight={null}
 							axisBottom={null}
@@ -57,6 +65,7 @@ const Heatmap = ({ graphData, graphHeight }) => {
 									: null
 							}
 							cellBorderWidth={2}
+							enableLabels={false}
 						/>
 					</div>
 				))
