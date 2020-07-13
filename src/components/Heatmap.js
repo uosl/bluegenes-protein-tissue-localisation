7e1cc571909f85366cdaaa0b77d3a733612c43bc
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-13 22:35:50 +0530

    changed background to white with  text NA on cell with no data

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -65,8 +65,63 @@ const Heatmap = ({ graphData, graphHeight }) => {
 									: null
 							}
 							cellBorderWidth={1}
-							cellBorderColor="rgb(51,51,51)"
-							enableLabels={false}
+							cellBorderColor="rgb(51, 51, 51)"
+							labelTextColor="rgb(51, 51, 51)"
+							nanColor="#fff"
+							cellShape={({
+								data,
+								value,
+								x,
+								y,
+								width,
+								height,
+								color,
+								opacity,
+								borderWidth,
+								borderColor,
+								enableLabel,
+								textColor,
+								onHover,
+								onLeave,
+								onClick,
+								theme
+							}) => {
+								return (
+									<g
+										transform={`translate(${x}, ${y})`}
+										onMouseEnter={onHover}
+										onMouseMove={onHover}
+										onMouseLeave={onLeave}
+										onClick={e => onClick(data, e)}
+										style={{ cursor: 'pointer' }}
+									>
+										<rect
+											x={width * -0.5}
+											y={height * -0.5}
+											width={width}
+											height={height}
+											fill={color}
+											fillOpacity={opacity}
+											strokeWidth={borderWidth}
+											stroke={borderColor}
+											strokeOpacity={opacity}
+										/>
+										{enableLabel && (
+											<text
+												alignmentBaseline="central"
+												textAnchor="middle"
+												style={{
+													...theme.labels.text,
+													fill: textColor
+												}}
+												fillOpacity={opacity}
+											>
+												{isNaN(value) ? 'NA' : ''}
+											</text>
+										)}
+									</g>
+								);
+							}}
 						/>
 					</div>
 				))
